d898d8a71b024a9e30b3bbf12ca02799e651cc80
Ricky <uhrishabh@gmail.com>
2023-01-26 18:25:27 +0100

    Added Code for reading Tred and Monat Misinformation Dataset. Changed the code for producing score.

---
+++ read_trec.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from tqdm import tqdm
+#from check_sentence import check_sentence
+
+dataset=pd.read_csv('./data/train_qid.csv',index_col=0,sep=';')
+
+
+import string
+PUNCTUATIONS = string.punctuation.replace(".","")
+
+
+def remove_punctuation(text):
+  trans = str.maketrans(dict.fromkeys(PUNCTUATIONS, ' '))
+  return text.translate(trans)
+
+def remove_whitespaces(text):
+    return " ".join(text.split())
+
+def clean_en_text(text):
+  """
+  text
+  """
+  text = re.sub(r"[^0-9a-zA-Z(),!?\'`]", " ", text)
+  #text= re.sub(r'\d+', '',text)
+  text = remove_punctuation(text)
+  text = remove_whitespaces(text)
+  return text.strip().lower()
+
+
+import re
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
+digits = "([0-9])"
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(websites,"<prd>\\1",text)
+    text = re.sub(digits + "[.]" + digits,"\\1<prd>\\2",text)
+    if "..." in text: text = text.replace("...","<prd><prd><prd>")
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [clean_en_text(s.strip()) for s in sentences]
+    return sentences
+
+
+dataset['sentences'] = dataset.apply(lambda x: split_into_sentences(x['text']), axis=1)
+
+dataset.to_csv('./data/train_qid_sen.csv',sep=';',index=None) #h_embeddings
